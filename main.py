7725661dee2c578d904a9ff980e337c50f9f5385
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-05-30 02:38:43 +0000

    window_setting-test4-EntryBox

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class MyFirstWindow:
     def __init__(self, master):
         self.master = master
         self.master.title("My First Tkinter Window")
-        self.master.geometry("400x300")
+        #self.master.geometry("400x300")
         self.master.configure(bg="#FFFFFF", borderwidth=10, highlightbackground="#CCCCCC", highlightthickness=10, highlightcolor="#CCCCCC")
         self.background = "white"
         self.bold_font_12 = ("Arial", 12, "bold")
@@ -16,13 +16,18 @@ class MyFirstWindow:
         # Create the parent frame
         self.parent_frame = Frame(self.master, bg="lightgrey", borderwidth=2, relief="ridge")
         self.parent_frame.grid(padx=10, pady=10)
-        # Create a label widget using .grid
+        # row 0 Create a label widget using .grid
         self.label = Label(self.parent_frame, text="Hello, Tkinter!", font=self.bold_font_12)
-        self.label.grid(row=0, column=0, padx=20, pady=20)
+        self.label.grid(row=0, columnspan=2, padx=20, pady=20)
+        # row 1
+        self.entry_label = Label(self.parent_frame, text="This is a label", font=self.bold_font_12)
+        self.entry_label.grid(row=1, column=0, padx=20, pady=20)
+        entry_box = Entry(self.parent_frame, font=self.text_font_6)
+        entry_box.grid(row=1, column=1, pady=5)
 
         # Create a button widget using .grid
         self.button = Button(self.parent_frame, text="Click Me!", font=self.bold_font_12, command=self.on_button_click)
-        self.button.grid(row=1, column=0, padx=20, pady=10)
+        self.button.grid(row=2, columnspan=2, padx=20, pady=10)
 
     def on_button_click(self):
         print("Button Clicked!")
