7cd269e56c66d34e7f4b625e4af64037b6178853
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-06-06 05:12:28 +0000

    -test3(resize image & window)

--- main.py
+++ main.py
@@ -23,17 +23,15 @@ class MainWindow:
     # row 1 Load the image
     image_path = "images.png"
     image = PhotoImage(file=image_path)
-
     # Resize the image to roughly 50x50 pixels
-    image = image.subsample(image.width() // 50, image.height() // 50)
-
+    image = image.subsample(image.width() // 150, image.height() // 80)
     # Create a label to display the image
     self.image_label = Label(self.parent_frame, image=image)
     self.image_label.image = image  # Store a reference to the PhotoImage object
     self.image_label.grid(row=1, padx=20, pady=20)
     # row 2 text
     txt = "this program is created to help the Waka Ama club sort through their recorded files from the race to determine the associated winner of place. This app is only to be used by the Waka Ama club therefore a password will be needed to access this app. if you were kind of club and you have not received the password please contact {phone number}."
-    self.text_label = Label(self.parent_frame, text=txt, font=("Arial", "10"),wraplength=250, bg=self.background)
+    self.text_label = Label(self.parent_frame, text=txt, font=("Arial", "8"), wraplength=350, bg=self.background)
     self.text_label.grid(row=2)
     # row 4
     self.to_password_button = Button(self.parent_frame, text="Submit", command=self.to_password)
