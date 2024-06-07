7b0169da1a3ffa687967a595ea5dfe538d3eba53
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-06-07 22:12:05 +0000

    -test3(find file_names) <successful>

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
-directory_to_search = 'waka_ama_db' # folder name input
-file_pattern = '*Final*' # file name input
-directory = directory_to_search
-filename = file_pattern
-for root, _, files in os.walk(directory):
+import fnmatch
+
+directory_to_search = 'waka_ama_db'  # folder name input
+file_pattern = '*Final*'  # file name input
+
+for root, _, files in os.walk(directory_to_search):
     for filename in files:
-        print(file)
+        if fnmatch.fnmatch(filename, file_pattern): #pattern matching
+            print(filename)
