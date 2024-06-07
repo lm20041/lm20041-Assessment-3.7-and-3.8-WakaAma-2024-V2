3201c40be78d7c497ed92cae62945da3e2824a76
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-06-07 22:09:04 +0000

    -test2
    I try seeing if the "import os" would work by itself but it needs "import fnmatch" to search for a patent within the folder and the bringing out those Pacific file names

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 directory_to_search = 'waka_ama_db' # folder name input
+file_pattern = '*Final*' # file name input
 directory = directory_to_search
+filename = file_pattern
 for root, _, files in os.walk(directory):
     for filename in files:
         print(file)
