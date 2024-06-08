32f875165d076ac2b104b4434280167d60c79524
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-06-08 02:49:51 +0000

    -test11(contacts)
    contacts of file successfully showing now we know as a fact that on the first line it displays the label of the file and underneath this place the data part five should have the number place in part five should have the regional association

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ import random
 
 directory_to_search = 'waka_ama_db'  # folder name input
 file_pattern = '*Final*'  # file name input
-matching_files = {}
+matching_files = []
+
 for root, _, files in os.walk(directory_to_search):
     for filename in files:
         if fnmatch.fnmatch(filename, file_pattern):
             file_path = os.path.join(root, filename)
-            print(file_path)
             matching_files.append(file_path)
 
-file_path = random.choice(matching_files)
-with open(file_path, 'r') as file:
-    print(file) 
+if matching_files:
+    file_path = random.choice(matching_files)
+    print(f"\nRandomly Selected File: {file_path}\n")
+
+    with open(file_path, 'r') as file:
+        content = file.read()
+        print("File Content:\n")
+        print(content)
+else:
+    print("No files found with the specified pattern.")
