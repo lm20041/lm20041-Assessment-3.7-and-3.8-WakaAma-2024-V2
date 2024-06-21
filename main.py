6f434a3dc4b985360f8a605bd997458d938217a2
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-06-21 22:18:45 +0000

    V3-CanvasTable-test1()  <<<(!)>>>
    -<<<After revaluation I decided to take things one step at a time we will start by inputting data and once we have that figure out we will switch to calculating that data diary out of files in folder, then we will learn how to input matching files in all file types into text box,  after that it's saving data results 2 folder as a file, and there's a bonus in text box will have an entry box to search up file names and a link to every file name and text box to open up a window displaying data.>>>
    error: -I built this starter version in my row and it seems I forgot to add self. to some values.

--- main.py
+++ main.py
@@ -12,10 +12,13 @@ data = {
     'Points': [50, 70, 80, 60, 90, 30, 40, 85, 95, 65]
 }
 
-class Convertor:
-    def __init__(self, master):
+class CanvasTable:
+    def __init__(self, master, data):
+        # input var's
         self.master = master
-        self.master.title("Entry Password")
+        self.data = data
+        # setting var's
+        self.master.title("CanvasTable")
         self.master.configure(bg="#FFFFFF", borderwidth=5, highlightbackground="#CCCCCC", highlightthickness=10, highlightcolor="#CCCCCC")
         self.text_font_6 = ("Arial", "12", "bold")
         self.text_fg = "#FFFFFF"
@@ -26,24 +29,33 @@ class Convertor:
         self.create_widgets()
 
     def create_widgets(self):
-        self.canvas = Canvas(self.parent_frame, bg="white")
+        button_fg = "white"
+        button_bg = "#004C99"
+        #frames
+        self.table_frame = Frame(parent_frame, bg=self.background)
+        self.table_frame.grid(row=1, column=0,padx=10 , pady=10)
+        self.file_frame = Frame(parent_frame, bg=self.background)
+        self.file_frame.grid(row=1, column=1,padx=10 , pady=10)
+        # create_child_widgets
+        self.create_table()
+        self.create_file_screen()
+#<<<<<        table_widgets        >>>>>
+    def create_table_widgets(self):
+        self.canvas = Canvas(self.table_frame, bg="white")
         self.canvas.pack(expand=True, fill=BOTH)
-
+         # table var's
         self.row_height = 30
         self.column_widths = [60, 100, 60]
-        self.headers = ['Place', 'Associate', 'Points']
-
-        self.create_table(data)
-
-    def create_table(self, data):
+        self.heading = 'full culb points'
+        self.headers = ['Place', 'Associate', 'Total Points']
+        # draw table
         self.draw_extra_row()
         self.draw_headers()
 
         for i, (place, associate, points) in enumerate(zip(data['place'], data['Associate'], data['Points'])):
             y = (i + 2) * self.row_height  # Adjust y position by +2 to account for extra row and headers
-            self.draw_row(y, place, associate, points)
-
-    def draw_extra_row(self):
+            self.draw_8_rows(y, place, associate, points)
+    def draw_extra_row():
         x_start = 0
         y_start = 0
         x_end = sum(self.column_widths)
@@ -51,22 +63,22 @@ class Convertor:
 
         self.canvas.create_rectangle(x_start, y_start, x_end, y_end, fill="lightgreen", outline="black", width=1)
         self.canvas.create_text(x_end / 2, y_end / 2, text="Extra Row", font=("Arial", 10, "bold"))
-
-    def draw_headers(self):
+    def draw_3_header():
         for col, header in enumerate(self.headers):
             x = sum(self.column_widths[:col])
             self.canvas.create_rectangle(x, self.row_height, x + self.column_widths[col], 2 * self.row_height, fill="lightgray", outline="black", width=1)
             self.canvas.create_text(x + self.column_widths[col] / 2, 1.5 * self.row_height, text=header, font=("Arial", 10, "bold"))
         self.canvas.create_line(0, 2 * self.row_height, sum(self.column_widths), 2 * self.row_height, fill="black")
-
-    def draw_row(self, y, place, associate, points):
+    def draw_8_rows(self, y, place, associate, points):
         for col, value in enumerate([place, associate, points]):
             x = sum(self.column_widths[:col])
             self.canvas.create_rectangle(x, y, x + self.column_widths[col], y + self.row_height, fill="lightgray", outline="black", width=1)
             self.canvas.create_text(x + self.column_widths[col] / 2, y + self.row_height / 2, text=value, font=("Arial", 10))
-
-
+#<<<<<      file_widgets       >>>>>
+    def create_file_widgets(self): 
+        self.text_box = Text(file_frame, width=40, height=10)
+        self.text_box.pack(pady=20, padx=20)
 if __name__ == "__main__":
     root = Tk()
-    app = Convertor(root)
+    app = CanvasTable(root, data)
     root.mainloop()
