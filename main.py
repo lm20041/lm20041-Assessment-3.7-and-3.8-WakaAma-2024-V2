f5fa740f408480e2ddd06bd311913259cc06476b
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-06-25 07:17:10 +0000

    -test6(convertor)
    error: config to_convertor_button which does not exist since there's no window before this yet is state=DISABLED

--- main.py
+++ main.py
@@ -4,56 +4,65 @@ from tkinter import *
 from functools import partial
 
 class Convertor:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("Entry Password")
-        self.master.configure(bg="#FFFFFF", borderwidth=5, highlightbackground="#CCCCCC", highlightthickness=10, highlightcolor="#CCCCCC")
-        self.text_font_6 = ("Arial", "12", "bold")
-        self.text_fg = "#FFFFFF"
-        self.background = "white"
-        self.parent_frame = Frame(self.master, bg=self.background)
-        self.parent_frame.grid(padx=10, pady=10)
+  def __init__(self, partner):
+    #vars
+    self.text_font_6 = ("Arial", "12", "bold")
+    self.text_fg = "#FFFFFF"
+    self.background = "white"
+    self.convertor_box = Toplevel()
 
-        self.create_widgets()
+    # disable to_convertor button
+    partner.to_convertor_button.config(state=DISABLED)
 
-    def create_widgets(self):
-        button_fg = "white"
-        button_bg = "#004C99"
+    # If users press cross at top, closes convertor and 'releases' password button
+    self.convertor_box.protocol('WM_DELETE_WINDOW', partial(self.close_password, partner))
 
-        self.heading_label = Label(self.parent_frame, text="Welcome to Waka Ama", font=self.text_font_6, bg=self.background)
-        self.heading_label.grid(row=0, column=0, columnspan=2, pady=10)
+    self.parent_frame = Frame(self.convertor_box, bg=self.background)
+    self.parent_frame.grid(padx=10, pady=10)
 
-        directory_to_search = 'waka_ama_db'
-        file_pattern = '*Final*'
+    self.create_widgets(partner)
+  def create_widgets(self, partner):
+    #var
+    button_fg = "white"
+    button_bg = "#004C99"
+    # Define entry labels
+    entry_labels = ["folder name:", "file name:"]
+    self.entry_boxes = []
+    # row0
+    self.heading_label = Label(self.parent_frame, text="Convertor", font=self.text_font_6, bg=self.background)
+    self.heading_label.grid(row=0, columnspan=2)
+    # row1
+    txt="please enter folder name and file name so we can begin. please press check to see if this information is valid. if it is press reults."
+    self.text_label = Label(self.parent_frame, text=txt, font=("Arial", "10"), bg=self.background)
+    self.text_label.grid(row=1, columnspan=2)
+    # row2 Create entry labels and boxes
+    for i, label in enumerate(entry_labels):
+      # Create and place the label above the entry box
+      Label(self.parent_frame, text=label, font=self.text_font_6, bg=self.background).grid(row=i+2, column=0, sticky=W, padx=5, pady=(10, 0))
+      entry_box = Entry(self.parent_frame, font=self.text_font_6)
+      show_char = '*' if label == "password" else ''
+      entry_box.grid(row=i+2, column=1, pady=5)
+      entry_box.config(highlightthickness=2, show=show_char, highlightbackground="grey", highlightcolor="blue")
 
-        self.file_type_all = self.create_file_type_all(directory_to_search)
-        self.file_type_match = self.create_file_type_match(self.file_type_all, file_pattern)
-
-        self.to_resultsexport_button = Button(self.parent_frame, width=8, height=1, text="Check", bg=button_bg, fg=button_fg, font=self.text_font_6, command=lambda: self.to_resultsexport(self.file_type_all, self.file_type_match))
-        self.to_resultsexport_button.grid(row=5, column=0, pady=10)
-        self.help_button = Button(self.parent_frame, width=8, height=1, text="Help", bg="#F4A434", fg=button_fg, font=self.text_font_6, command=self.to_help)
-        self.help_button.grid(row=5, column=1, pady=10)
-
-    def create_file_type_all(self, folder):
-        file_type_all = {}
-        for root, _, files in os.walk(folder):
-            for filename in files:
-                file_path = os.path.join(root, filename)
-                file_type_all[filename] = file_path
-        return file_type_all
-
-    def create_file_type_match(self, file_dict, pattern):
-        file_type_match = {}
-        for filename in fnmatch.filter(file_dict.keys(), pattern):
-            file_path = file_dict[filename]
-            file_type_match[filename] = file_path
-        return file_type_match
-
-    def to_resultsexport(self, file_all, file_match):
-        ResultsExport(self, file_all, file_match)
-
-    def to_help(self):
-        pass
+      self.entry_boxes.append(entry_box)
+    # row4
+    self.error_label = Label(self.parent_frame, text="", font=("Arial", "10"),wraplength=400, bg=self.background, fg="red")
+    self.error_label.grid(row=4, columnspan=2)
+    # row5 Create buttons
+    self.check_button = Button(self.parent_frame, width=8, height=1, text="check", bg=button_bg, fg=button_fg, font=self.text_font_6, command=self.open_filepath)
+    self.check_button.grid(row=5, ccolumn=0)
+    self.help_button = Button(self.parent_frame, width=8, height=1, text="Help", bg="#F4A434", fg=button_fg, font=self.text_font_6, command=self.validate_entries)
+    self.help_button.grid(row=5, ccolumn=1)
+  def open_filepath():
+    pass
+  def list_filepath():
+    pass
+  def to_help(self, partner):
+    pass
+  def close_Convertor(self, partner):
+    #put to_convertor button back to normal
+    partner.to_convertor_button.config(state=NORMAL)
+    self.convertor_box.destroy()
 if __name__ == "__main__":
     root = Tk()
     app = Convertor(root)
