0d41e3422cf60891b7b59d04772bc3bd3cfa5414
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-06-25 07:01:20 +0000

    -test3(class main/password)
    -line54 Forgot to put parent frame on password box

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
-
-class Password:
+from functools import partial
+# <<<< class MainWindow >>>>
+class MainWindow:
   def __init__(self, master):
     self.master = master #Using this in windows settings to allow easy placement of frames
     self.master.title("Entry Password")
@@ -8,6 +9,48 @@ class Password:
     self.text_font_6 = ("Arial", "12", "bold")
     self.text_fg = "#FFFFFF"
     self.background = "white"
+    self.parent_frame = Frame(self.master, bg=self.background)
+    self.parent_frame.grid(padx=10, pady=10)
+
+    self.create_widgets()
+  def create_widgets(self):
+    #var
+    button_fg = "white"
+    button_bg = "#004C99"
+    # row 0 text
+    self.heading_label = Label(self.parent_frame, text="Welcome to Waka Ama", font=self.text_font_6, bg=self.background)
+    self.heading_label.grid(row=0)
+
+    # row 1 Load the image
+    image_path = "images.png"
+    image = PhotoImage(file=image_path)
+    # Resize the image to roughly 50x50 pixels
+    image = image.subsample(image.width() // 150, image.height() // 80)
+    # Create a label to display the image
+    self.image_label = Label(self.parent_frame, image=image)
+    self.image_label.image = image  # Store a reference to the PhotoImage object
+    self.image_label.grid(row=1, padx=20, pady=20)
+    # row 2 text
+    txt = "this program is created to help the Waka Ama club sort through their recorded files from the race to determine the associated winner of place. This app is only to be used by the Waka Ama club therefore a password will be needed to access this app. if you were kind of club and you have not received the password please contact {phone number}."
+    self.text_label = Label(self.parent_frame, text=txt, font=("Arial", "8"), wraplength=350, bg=self.background)
+    self.text_label.grid(row=2)
+    # row 4
+    self.to_password_button = Button(self.parent_frame, width=8, height=1, text="Start", font=self.text_font_6, bg=button_bg, fg=button_fg, command=self.to_password)
+    self.to_password_button.grid(row=4, pady=10)
+  def to_password(self):
+    Password(self)
+# <<<< class password >>>>
+class Password:
+  def __init__(self, partner):
+    # Var's
+    self.text_font_6 = ("Arial", "12", "bold")
+    self.text_fg = "#FFFFFF"
+    self.background = "white"
+    # add <partner>
+    self.password_box = Toplevel()
+    partner.to_password_button.config(state=DISABLED)
+    self.password_box.protocol('WM_DELETE_WINDOW', partial(self.close_password, partner))
+    # parent_frame
     self.parent_frame = Frame(self.master, bg=self.background)
     self.parent_frame.grid(padx=10, pady=10)
 
@@ -83,7 +126,10 @@ class Password:
       self.error_label.config(text=error_message, fg="red")
       for entry_box in self.entry_boxes:
         entry_box.config(bg="red", highlightbackground="pink", highlightcolor="black")
+  def close_password(self, partner):
+    partner.to_password_button.config(state=NORMAL)
+    self.password_box.destroy()
 if __name__ == "__main__":
   root = Tk()
-  app = Password(root)
+  app = MainWindow(root)
   root.mainloop()
