0c8d7b12b9c31148740df4efa1027f7c3de7ce51
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-06-27 22:01:57 +0000

    -test2(read files content)
    AttributeError: 'Convertor' object has no attribute 'file_contents'
    this may be because of what type of file it is trying to read

--- main.py
+++ main.py
@@ -7,28 +7,27 @@ from functools import partial
 class Convertor:
     def __init__(self):
         self.open_filepath()
+        self.file_contents = {}
 
     def open_filepath(self):
-        folder_name = 'waka_ama_db' #self.entry_boxes[0].get()
-        file_name = '*Final*' #self.entry_boxes[1].get()
-        #> record all and  matching file lists and their pathway
+        folder_name = 'waka_ama_db' # self.entry_boxes[0].get()
+        file_name = '*Final*' # self.entry_boxes[1].get()
+        # Record all and matching file lists and their pathways
         # file_type_all
         self.file_type_all = self.create_file_type_all(folder_name)
-        # check
+        # Check
         if not self.file_type_all:
             print("No matching files found.")
-    
+
         # file_type_match
         self.file_type_match = self.create_file_type_match(self.file_type_all, file_name)
-        # check
+        # Check
         if not self.file_type_match:
             print("No matching files found.")
         else:
-            # activate calculate file data <<<<<
-            print(self.file_type_all)
-            print("\n\n\n\n\n\n\n")
-            print(self.file_type_match)
-            
+            # Activate calculate file data <<<<<
+            self.read_files()
+
     def create_file_type_all(self, folder):
         file_type_all = {}
         for root, _, files in os.walk(folder):
@@ -36,12 +35,21 @@ class Convertor:
                 file_path = os.path.join(root, filename)
                 file_type_all[filename] = file_path
         return file_type_all
+
     def create_file_type_match(self, file_dict, pattern):
         file_type_match = {}
         for filename in fnmatch.filter(file_dict.keys(), pattern):
             file_path = file_dict[filename]
             file_type_match[filename] = file_path
         return file_type_match
+
+    def read_files(self):
+        for filename, filepath in self.file_type_match.items():
+            with open(filepath, 'r') as file:
+                self.file_contents[filename] = file.read()
+        print("\nFile contents:\n")
+        for filename, content in self.file_contents.items():
+            print(f"Contents of {filename}:\n{content}\n")
 
 if __name__ == "__main__":
     root = Tk()
